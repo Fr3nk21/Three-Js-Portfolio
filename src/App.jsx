b7578fb30e1a6bddf0c40d2ac8bf9cb9269ebe56
Fr3nk21 <104112709+Fr3nk21@users.noreply.github.com>
2024-01-15 21:50:20 +1100

    Island Set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { fromJSON } from "postcss"
 import { Route, BrowserRouter as Router, Routes } from
 'react-router-dom'
 
@@ -13,7 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Project />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
